3d7d3d050e82af225543621b62567ad3110c81ff
vudovenko <vladislav.udovenko0@yandex.ru>
2024-07-21 22:58:39 +0500

    Criteria API

--- src/main/java/ru/javabegin/hibernate/Main.java
+++ src/main/java/ru/javabegin/hibernate/Main.java
@@ -1,51 +1,48 @@
 package ru.javabegin.hibernate;
 
+
+
+import jakarta.persistence.Query;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import lombok.extern.log4j.Log4j2;
 import org.hibernate.Session;
 import ru.javabegin.hibernate.entity.User;
 
+import java.util.List;
+
+@Log4j2
 public class Main {
 
     public static void main(String[] args) {
 
+        log.info("Hibernate tutorial started");
+
         //сразу получаем готовый SessionFactory и сразу создаем сессию
         Session session = HibernateUtil.getSessionFactory().openSession();
 
-        // открыть транзакцию
-        session.getTransaction().begin();
+        // подготовка запроса - получение всех пользователей
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
+        Root<User> root = criteriaQuery.from(User.class);
+        criteriaQuery.select(root); // конечный запрос select из таблицы User
 
-        User user = new User(); // состояние NEW (transient)
-        user.setEmail("newf2roma10ppfdsd8@email.com");
-        user.setUsername("nsdewf10rofsdsdmapp8");
-        user.setPassword("2asdfsdfsdf");
-        // id не заполняем, т.к. он сгенерится автоматически в БД и присвоится в поле
+        Predicate p1 = criteriaBuilder.gt(root.get("id"), 10000);
+        Predicate p2 = criteriaBuilder.lt(root.get("id"), 20000);
 
-        session.persist(user); // состояние managed (persistent) - управляемый объект - контейнером Hibernate
+        criteriaQuery.select(root).where(criteriaBuilder.and(p1 ,p2));
 
-        session.getTransaction().commit(); // сохранить изменения (транзакция завершается)
+        // выполнение запроса с получением результата
+        Query query = session.createQuery(criteriaQuery);
+        List<User> users = query.getResultList();
 
-        System.out.println("user.getId() = " + user.getId());
+        // log.info(users); // если у вас в таблице много пользователей, то при выводе их в консоль, может немного подтормаживать
+        log.info("users count = "+users.size()); // можно вывести просто кол-во полученных данных, чтобы не заполнять всю консоль пользователями
 
         session.close();// закрыть сессию
 
-        // состояние detached
-
-        session = HibernateUtil.getSessionFactory().openSession();
-
-        // открыть транзакцию
-        session.getTransaction().begin();
-
-        user.setEmail("10@mail.ru");
-        user.setUsername("us10");
-
-        // т.к. мы хотим обновить уже существующий объект в БД, то вместо persist используем метод merge,
-        // который по id находит запись и обновляет для нее новые данные
-        // При попытке вызвать persist - вы получите ошибку "detached entity passed to persist", т.к. User создавался еще в прошлой сессии
-        session.merge(user); //  заново присоединяем ранее созданный объект User
-
-        System.out.println("user.getId() = " + user.getId());
-
-
-        session.getTransaction().commit();
         HibernateUtil.close(); // закрыть Session Factory
     }
 
